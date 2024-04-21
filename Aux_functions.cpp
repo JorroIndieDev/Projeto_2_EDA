9f299347b3e1e04df2ed0c2a3f4762404db71645
Jorro <142111666+JorroIndieDev@users.noreply.github.com>
2024-04-21 16:33:22 +0100

    foundation for saving struct Airplane

--- Aux_functions.cpp
+++ Aux_functions.cpp
@@ -55,3 +55,65 @@ int random_range(int lower, int upper){
     int num = (rand() % (upper - lower + 1)) + lower;
     return num;
 }
+
+
+// TODO change everything for the airport
+void SaveToFile(std::string file_name) {
+
+    std::ofstream outfile;
+    struct customer input;
+
+    // open Accounts file for writing
+    outfile.open("accounts.dat");
+
+    if (!outfile.is_open()) {
+        std::cout<< "\nError opening accounts.dat\n\n";
+        exit(1);
+    }
+
+    // instructions to user
+    printf("Enter \"stop\" for First Name to end program.");
+
+    // endlessly read from keyboard and write to file
+    while (1) {
+        // prompt user
+        printf("\nFirst Name: ");
+        scanf("%s", input.fname);
+        // exit if no name provided
+        if (strcmp(input.fname, "stop") == 0)
+            break;
+        // continue reading from keyboard
+        printf("Last Name : ");
+        scanf("%s", input.lname);
+        printf("Acct Num  : ");
+        scanf("%d", &input.acct_num);
+        printf("Balance   : ");
+        scanf("%f", &input.acct_balance);
+
+        // write entire structure to Accounts file
+        outfile.write(reinterpret_cast <char *> (&input), sizeof(struct customer));
+    }
+    outfile.close();
+    outfile.clear();
+}
+// TODO change everything for the airport
+void LoadFromFile(std::string file_name){
+    std::ifstream infile;
+    struct customer input;
+
+    //open the accounts file
+    infile.open(file_name);
+    if (!infile.is_open()) {
+        std::cerr << "\nError opening accounts.dat\n\n";
+        infile.close();
+        infile.clear();
+        exit(1);
+    }
+
+/*    while (infile.read(reinterpret_cast <char *> (&input), sizeof(struct customer)))
+        printf("Name = %10s %10s   Acct Num = %8d   Balance = %8.2f\n",
+               input.fname, input.lname, input.acct_num, input.acct_balance);*/
+
+    infile.close();
+    infile.clear();
+}
--- main.cpp
+++ main.cpp
@@ -4,49 +4,24 @@
  * (watch video explaining and leave notes here)
  *
  */
+
 #include <iostream>
 #include "Headers/structs.h"
 #include "Headers/AirportListHandler.h"
 #include "Headers/Menus.h"
 #include "Headers/init_planes_people.h"
 #include "Headers/Aux_functions.h"
-/*#include <iostream>
-#include <fstream>
-#include <string>
 
-const int MAX_LINES = 100; // Define the maximum number of lines
 
 int main() {
-    std::ifstream file("../Data_Files/origem.txt"); // Replace "airports.txt" with your file name
 
-    if (!file.is_open()) {
-        std::cerr << "Failed to open file." << std::endl;
-        return 1;
-    }
+    SaveToFile("accounts.dat");
 
-    std::string lines[MAX_LINES];
-    std::string line;
-    int count = 0;
+    LoadFromFile("accounts.dat");
 
-    while (std::getline(file, line) && count < MAX_LINES) {
-        lines[count] = line;
-        count++;
-    }
+   /* srand(time(0));
 
-    file.close();
-
-    // Print the read lines
-    for (int i = 0; i < count; ++i) {
-        std::cout << lines[i] << std::endl;
-    }
-
-    return 0;
-}*/
-int main() {
-
-    srand(time(0));
-
-    file_data fileData;
+    file_data fileData = file_data();
 
     load_files_to_mem(fileData);
 
@@ -57,7 +32,7 @@ int main() {
         init_plane(airport,fileData);
     }
 
-    dayCycle(airport,fileData);
+    dayCycle(airport,fileData);*/
 
     return 0;
 }
