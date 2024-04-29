4c2304b59f175a1b48309df5428a105e081904f0
Jorro <142111666+JorroIndieDev@users.noreply.github.com>
2024-04-29 15:38:49 +0100

    Deleted Test notes

--- main.cpp
+++ main.cpp
@@ -14,84 +14,10 @@
 
 
 int main(int argc, char *argv[]) {
-/*
-TODO Links de referencia:
- -https://www.youtube.com/watch?v=jDM6_TnYIqE
- -https://www.sanfoundry.com/c-program-create-balanced-binary-tree-incoming-data/
-
- Notas: as funcoes nao estao todas as necessarias, falta remover e uma funcao dedicada a ordenar
- tecnicamente e so remover parte da insert_tree_node e temos a ordenacao mas e melhor testarmos em cunjunto, se acharem
- que conseguem estejam a vontade, nao se esquecam de comentar as partes importantes tentei deixar tudo o que fiz e fizemos
- comentado a explicar.
-
-TODO Facam o vosso teste aqui e chamem as funcoes no AirportListHandler.cpp|.h
-     as funcoes para a arvore sao as seguintes: (se nao conseguirem deixem me mensagem - Jorro)
-
-// tree handler functions
-
-// make a node with the passenger
-Nacionality::Pass_tree * new_tree_node(struct passenger &passenger);
-
-// rotate tree to the right
-Nacionality::Pass_tree * rotate_node_to_right(Nacionality::Pass_tree * tree_node);
-
-// rotate tree to the left
-Nacionality::Pass_tree * rotate_node_to_left(Nacionality::Pass_tree * tree_node);
-
-// insert node in tree
-Nacionality::Pass_tree * insert_tree_node(Nacionality::Pass_tree * tree_node, struct passenger &passenger);
-
-// calculate node height
-int node_height(Nacionality::Pass_tree * tree_node);
-
-// DISCART FUNCTION
-// funcao vai dar print da arvore
-void preOrder(Nacionality::Pass_tree * root);
-
-// get node balance
-int get_node_balance(Nacionality::Pass_tree * tree_node);
-*/
-
-/***********************************************
- *
- * Facam aqui e testem
- *
- ***********************************************/
 
     srand(time(0));
-    file_data fileData = file_data();
 
-    load_files_to_mem(fileData);
-
-    Airport airport = init_airport();
-
-    airport.nacionality_head->nacionality = ' ';
-    airport.nacionality_head->next_nacionality = nullptr;
-    airport.nacionality_head->root_passenger = nullptr;
-    init_plane(airport,fileData);
-    Plane::passenger_in_plane * temp = airport.head_arrv->plane.head_passenger;
-    airport.nacionality_head->root_passenger = insert_tree_node(airport.nacionality_head->root_passenger,
-                                                                temp->passenger);
-    while (temp != NULL){
-        airport.nacionality_head->root_passenger = insert_tree_node(airport.nacionality_head->root_passenger,
-                                                                    temp->passenger);
-        temp = temp->next_passenger;
-    }
-
-    print2DUtil(airport.nacionality_head->root_passenger,0);
-    std::cout << "SPINE\n";
-    make_spine(airport.nacionality_head->root_passenger);
-    print2DUtil(airport.nacionality_head->root_passenger,0);
-    std::cout << "BALENCED\n";
-    airport.nacionality_head->root_passenger = balance_tree(airport.nacionality_head->root_passenger);
-    print2DUtil(airport.nacionality_head->root_passenger,0);
-/***********************************************
- *
- *
- ***********************************************/
-
-
-    /*// init rand seed
+    // init rand seed
     srand(time(0));
 
     // creating struct for holding all Data files
@@ -119,7 +45,7 @@ int get_node_balance(Nacionality::Pass_tree * tree_node);
     }
 
     // initialize days / cylces of the airport
-    dayCycle(airport,fileData);*/
+    dayCycle(airport,fileData);
 
     return 0;
 }
